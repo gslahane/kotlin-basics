16cc554e2e5cb5ccb0036c4c7c7e83276cb30fd6
G Lahane <gslahane@outlook.com>
2019-04-22 11:08:03 +0530

    Initial commit

---
+++ src/tidbits/Computer.kt
@@ -0,0 +1,18 @@
+package tidbits
+
+open class Computer {
+    fun sayComputer() {
+        println("I'm computer")
+    }
+}
+
+class TabletComputer: Computer(){
+    fun sayTabletComputer() {
+        println("I'm a tablet computer")
+    }
+}
+
+
+
+fun main(args: Array<String>) {
+}
